45439940b07a14022a831cf0abeaefa65327b80f
Stuart Berg <bergs@janelia.hhmi.org>
2018-06-10 10:31:29 -0400

    conda-recipe: Use Anaconda's g++ package on Linux

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,6 +3,13 @@
 mkdir -p build-conda
 cd build-conda
 
+if [[ $(uname) == 'Linux' ]]; then
+	# Check which gcxx abi to use; for compatibility with libs build with gcc < 5:
+	if [[ ${DO_NOT_BUILD_WITH_CXX11_ABI} == '1' ]]; then
+	    CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0 ${CXXFLAGS}"
+	fi
+fi
+
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
