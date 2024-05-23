109a9b993f9396d6de1e1db3edf1384a2ddfc9cf
Stuart Berg <bergs@janelia.hhmi.org>
2024-05-22 22:10:58 -0400

    conda-recipe: Added some commented-out build variables related to address-sanitizer

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -3,6 +3,23 @@
 mkdir -p build-conda
 cd build-conda
 
+# CFLAGS="${CFLAGS} -fsanitize=address"
+# CXXFLAGS="${CXXFLAGS} -fsanitize=address"
+# LDFLAGS="${LDFLAGS} -fsanitize=address"
+
+# if [[ $(uname) == "Darwin" ]]; then
+#     CFLAGS="${CFLAGS} -shared-libasan"
+#     CXXFLAGS="${CXXFLAGS} -shared-libasan"
+#     LDFLAGS="${LDFLAGS} -shared-libasan"
+# fi
+
+## When testing with address-sanitizer, you'll need the following in your test script:
+# if [[ $(uname) == "Darwin" ]]; then
+#     export DYLD_INSERT_LIBRARIES=${PREFIX}/lib/clang/*/lib/darwin/libclang_rt.asan_osx_dynamic.dylib
+# else
+#     export LD_PRELOAD=${PREFIX}/lib/libasan.so.5
+# fi
+
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=${PREFIX} \
