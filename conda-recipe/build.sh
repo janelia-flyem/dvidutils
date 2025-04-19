09bf0a3683f22e021eda46424c5fc29c9087e237
Stuart Berg <bergs@janelia.hhmi.org>
2025-04-19 09:13:28 -0400

    cmake: Letting cursor modernize CMakeLists.txt when it comes to finding python and numpy

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -28,10 +28,11 @@ cmake .. \
     -DCMAKE_CXX_LINK_FLAGS="${LDFLAGS}" \
     -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
     -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+    -DCMAKE_OSX_ARCHITECTURES="$(uname -m)" \
     -DBOOST_ROOT="${PREFIX}" \
     -DBoost_LIBRARY_DIR="${PREFIX}/lib" \
     -DBoost_INCLUDE_DIR="${PREFIX}/include" \
-    -DCMAKE_OSX_ARCHITECTURES="$(uname -m)" \
+    -DPython_ROOT_DIR=${PREFIX} \
 ##
 
 VERBOSE=1 make -j${CPU_COUNT} dvidutils
